34842a96801bc211ed5342f761218b3cd260b218
Kei Takashima <kei.takashima@crssoftware.co.nz>
2022-10-02 08:51:24 +1300

    add tailwind

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
-import { BrowserRouter, Routes, Route, Link } from 'react-router-dom'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
 import reportWebVitals from './reportWebVitals'
 import { Home } from './Home'
